c2976e7877cee6d316f83393b9563fb4d3c811ab
Azka Full Snack Developer:) <82513502+azkadev@users.noreply.github.com>
2022-10-12 12:32:47 +0000

    update

---
+++ dart/install.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+sudo apt update -y
+sudo apt-get install apt-transport-https -y
+sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
+sudo sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
+sudo apt update -y
+sudo apt-get install dart -y
---
+++ google-chrome/install.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo add-apt-repository "deb http://dl.google.com/linux/chrome/deb/ stable main"
+sudo apt update -y
+sudo apt install google-chrome-stable -y
